1504e0d6348a1f8e2b059b15c48086fce46fbe77
Nathan Strady <145336391+NathanStrady@users.noreply.github.com>
2025-02-13 07:31:19 +0100

    Start of skills sections

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -5,8 +5,33 @@ import React from "react";
 const Skills = () => {
     return (
         <SectionContainer id="skills">
-            <div>
+            <div className="flex flex-col space-y-10"  >
                 <SectionHeader text="Mes" highlightText="Compétences"/>
+                <div className="card p-10 ">
+                    <div className="items-center space-y-20">
+                        <h1 className="text-start font-semibold text-[20px] md:text-[35px] md:inline">
+                            Outils maîtrisés
+                        </h1>
+                        <div className="flex flex-wrap items-center bg-primary w-full">
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-start">
+                        <h1 className="text-start font-semibold text-[20px] md:text-[35px] md:inline">
+                            Hard-Skills
+                        </h1>
+                        <div className="flex flex-wrap items-center bg-primary w-full">
+
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-start space-y-10">
+                        <h1 className="text-start font-semibold text-[20px] md:text-[35px] md:inline">
+                            Soft-Skills
+                        </h1>
+                        <div className="flex flex-wrap items-center bg-primary w-full">
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </SectionContainer>
     )
